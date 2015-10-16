b765ed03dcf2875c0531fd2e107b896fb2b37d05
knolegsb <seon2you@hotmail.com>
2015-10-16 02:36:57 -0700

    adding data collection

--- WpfStockChart/DataCollection.cs
+++ WpfStockChart/DataCollection.cs
@@ -1,6 +1,34 @@
-﻿namespace WpfStockChart
+﻿using System.Collections.Generic;
+
+namespace WpfStockChart
 {
-    internal class DataCollection
+    public class DataCollection
     {
+        private List<DataSeries> dataList = new List<DataSeries>();
+        private StockChartTypeEnum stockChartType = StockChartTypeEnum.HiLoOpenClose;
+
+        public StockChartTypeEnum StockChartType
+        {
+            get { return stockChartType; }
+            set { stockChartType = value; }
+        }
+
+        public List<DataSeries> DataList
+        {
+            get { return dataList; }
+            set { dataList = value; }
+        }
+
+        //public void AddStockChart(ChartStyle cs)
+        //{
+
+        //}
+        public enum StockChartTypeEnum
+        {
+            HiLo = 0,
+            HiLoOpenClose = 1,
+            Candle = 2, 
+            Line = 3
+        }
     }
 }
